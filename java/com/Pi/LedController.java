080fba6e3b36a6dfd3c87ec6b54d0952b16cf4bf
DTUS1 <DSCN4758@gmail.com>
2015-11-28 04:00:44 -1000

    Stuff

---
+++ java/com/Pi/LedController.java
@@ -0,0 +1,36 @@
+package com.Pi;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.RaspiPin;
+
+public class LedController {
+	
+	private static GpioPinDigitalOutput pin;
+	
+	@RequestMapping("/")
+	public String hi() {
+		return "Hello World";
+	}
+
+	@RequestMapping("/light")
+	public String light() {
+		
+		if(pin == null) {
+			
+		
+		GpioController gpio = GpioFactory.getInstance();
+		pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "Led1", PinState.LOW);
+		
+		}
+		
+		pin.toggle();
+		
+		return "toggled";
+	}
+	
+}
